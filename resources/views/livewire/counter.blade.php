972194669616b504f0f5f7b3c74ce69ef31255d0
programinglive <75464690+programinglive@users.noreply.github.com>
2023-09-09 23:46:32 +0700

    - change order + - button counter

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,8 +1,8 @@
 <div class="card p-5 bg-light rounded w-25 mx-auto mt-5">
   <h1 class="text-center my-5">Livewire Sample</h1>
   <div class="text-center">
-    <button wire:click="increment" class="btn btn-primary">+</button>
     <button wire:click="decrement" class="btn btn-danger">-</button>
+    <button wire:click="increment" class="btn btn-primary">+</button>
     <p class="my-5 text-xl">{{ $count }}</p>
   </div>
 </div>
