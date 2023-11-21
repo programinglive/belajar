76a54c6aeebd33d616e4c96df9a76babaffd30f1
mahatmamahardhika <mahatma.mahardhika@programinglive.com>
2023-11-22 03:13:21 +0700

    add google id on model user

--- database/migrations/2023_11_21_190129_add_google_id_column.php
+++ database/migrations/2023_11_21_190129_add_google_id_column.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function($table) {
-            $table->integer('google_id');
+            $table->integer('google_id')->nullable();
         });
     }
 
