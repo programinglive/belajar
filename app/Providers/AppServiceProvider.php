5a314e9792b92759d4d1e6fdbf76653f98798bcb
programinglive <mahatmamahardhika200588@gmail.com>
2025-12-06 00:58:35 +0700

    fix: force https scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
     }
 }
